d203baad0e747107d32eaf56de6fae39c7e3104e
luffy981 <jhonsmith981@hotmail.com>
2021-10-23 11:31:30 -0500

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	int *vodka = NULL;
+	int *ron = NULL;
+	int flag = 0;
+	int tmp;
+
+	for (i = 0 ; i < size - 1 ; i++)
+	{
+		flag = 0;
+		for (j = 0 ; j < size - i - 1 ; j++)
+		{
+			vodka = &array[j];
+			ron = &array[j + 1];
+			if (*vodka > *ron)
+			{
+				tmp = *vodka;
+				*vodka = *ron;
+				*ron = tmp;
+				flag = 1;
+				print_array(array, size);
+			}
+		}
+		if (flag == 0)
+			break;
+	}
+
+}
