27ada054c7960dd217fa3deb3ebb97b132d286ca
Rayd Trujillo <rayd1893@gmail.com>
2021-10-23 22:03:53 +0000

    Add betty style

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,11 @@
 #include "sort.h"
 
-
-
+/**
+ * insertion_sort_list - Sort doubly linked list
+ * @list: Doubly linked list
+ *
+ * Return: Nothing
+ */
 void insertion_sort_list(listint_t **list)
 {
 
