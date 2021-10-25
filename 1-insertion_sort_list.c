c9525cf53d6a0c7b0b6d4e7506af3834949dc2d7
luffy981 <jhonsmith981@hotmail.com>
2021-10-25 12:14:14 -0500

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,33 +8,57 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *head = NULL;
+	listint_t *back_head = NULL;
+	listint_t *tmp = NULL;
 
-	listint_t *node = *list;
-	listint_t *hole = *list;
-	listint_t *save1 = NULL;
-	listint_t *save2 = NULL;
-	int tmp;
-
-	node = *list;
-	for (node = node->next ; node ; )
+	if (!list || !(*list) || (!((*list)->prev) && !((*list)->next)))
+		return;
+	head = *list;
+	head = head->next;
+	while (head)
 	{
-		save1 = hole;
-		save2 = node;
-		tmp = hole->n;
-		while (hole && hole->n > node->n)
+		back_head = head->prev;
+		if (back_head->n > head->n)
 		{
-			tmp = hole->n;
-			hole->n = node->n;
-			node->n = tmp;
-			if (hole->prev)
-				hole = hole->prev;
-			node = node->prev;
-			tmp = node->n;
+			swap_nodes(&back_head, &head, list);
 			print_list(*list);
+			while (back_head->prev)
+			{
+				if ((back_head->n) < (back_head->prev->n))
+				{
+					tmp = back_head->prev;
+					swap_nodes(&tmp, &back_head, list);
+					print_list(*list);
+				}
+				back_head = back_head->prev;
+			}
 		}
-		hole = save1;
-		node = save2;
-		hole = hole->next;
-		node = node->next;
+		head = head->next;
 	}
+}
+void swap_nodes(listint_t **turtle, listint_t **rabbit, listint_t **list)
+{
+	listint_t *before;
+	listint_t *after;
+
+	if (!(*turtle) || !(*rabbit))
+		return;
+	before = (*turtle)->prev;
+	after = (*rabbit)->next;
+
+	if (before)
+		before->next = (*rabbit);
+	(*rabbit)->prev = before;
+	(*rabbit)->next = (*turtle);
+	(*turtle)->prev = (*rabbit);
+	(*turtle)->next = after;
+
+	if (after)
+		after->prev = (*turtle);
+	*turtle = *rabbit;
+	*rabbit = (*turtle)->next;
+
+	if (!before)
+		*list = *turtle;
 }
