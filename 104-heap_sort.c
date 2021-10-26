6ffeb3a2165e39f22b2b29ba2ee777653dec1a88
luffy981 <jhonsmith981@hotmail.com>
2021-10-25 22:05:22 -0500

    heap sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 
-void max_heapify(int *array, size_t size, size_t i)
+void max_heapify(int *array, size_t size, size_t i, size_t print_size)
 {
 	int largest = i;
 	int left = 2 * i + 1;
@@ -20,8 +20,8 @@ void max_heapify(int *array, size_t size, size_t i)
 		tmp = array[largest];
 		array[largest] = array[i];
 		array[i] = tmp;
-		print_array(array, size);
-		max_heapify(array, size, largest);
+		print_array(array, print_size);
+		max_heapify(array, size, largest, print_size);
 	}
 
 }
@@ -30,22 +30,23 @@ void heap_sort(int *array, size_t size)
 {
 	int i;
 	int tmp;
+	size_t print_size = size;
 
 	if (size <= 1 || !array)
 		return;
 	for (i = (size / 2) - 1 ; i >= 0; i--)
 	{
-		max_heapify(array, size, i);
+		max_heapify(array, size, i, print_size);
 	}
 	for (i = size - 1 ; i >= 0 ; i--)
 	{
 		if (i != 0)
 		{
-			tmp = array[1];
-			array[1] = array[i];
+			tmp = array[0];
+			array[0] = array[i];
 			array[i] = tmp;
-			print_array(array, size);
-			max_heapify(array, size, 1);
+			print_array(array, print_size);
+			max_heapify(array, i, 0, print_size);
 		}
 
 	}
