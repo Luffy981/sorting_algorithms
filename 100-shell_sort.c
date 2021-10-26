e81d9eac55de6168d37a4b8aeb24dcf5b97c8626
luffy981 <jhonsmith981@hotmail.com>
2021-10-25 23:18:07 -0500

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -6,23 +6,27 @@ void shell_sort(int *array, size_t size)
 	int i;
 	size_t j;
 	int tmp;
+	if (size <= 1)
+		return;
+	while ((3 * gap + 1) <= size)
+		gap = gap * 3 + 1;
 
-	for (gap = size / 2 ; gap >= 1 ; gap = gap / 2)
+	for (; gap >= 1 ; gap = (gap - 1) / 3)
 	{
 		for (j = gap ; j < size ; j++)
 		{
-			for (i = j - gap ; i >= 0; i = i - gap)
+			for (i = j ; i >= (int)gap  ; i = i - gap)
 			{
-				if (array[i + gap] < array[i])
+				if (array[i - gap] < array[i])
 				{
 					tmp = array[i];
-					array[i] = array[i + gap];
-					array[i + gap] = tmp;
+					array[i] = array[i - gap];
+					array[i - gap] = tmp;
 				}
 				else
 					break;
 			}
 		}
-	print_array(array, size);
+		print_array(array, size);
 	}
 }
